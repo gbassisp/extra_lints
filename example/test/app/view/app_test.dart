6c68d13a6a861fc85a01475a4f79cfd1b1eb3292
gbassisp <guilherme.bassi.sp@gmail.com>
2023-05-28 21:04:31 +0930

    test: added case in example app

--- example/test/app/view/app_test.dart
+++ example/test/app/view/app_test.dart
@@ -1,5 +1,7 @@
 import 'package:example/app/app.dart';
 import 'package:example/counter/counter.dart';
+import 'package:example/l10n/l10n.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -7,6 +9,23 @@ void main() {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
       expect(find.byType(CounterPage), findsOneWidget);
+    });
+  });
+
+  group('linter', () {
+    test('expect lint', () {
+      // expect_lint: avoid_string_literals_inside_widget
+      final w = Builder(builder: (_) => const Text('Counter'));
+
+      // not_expect_lint: avoid_string_literals_inside_widget
+      final a = Builder(
+        builder: (context) => Text(
+          context.l10n.counterAppBarTitle,
+        ),
+      );
+
+      expect(a, isNotNull);
+      expect(w, isNotNull);
     });
   });
 }
