0b3e22c2cda031ef764d11186c74719304cf8e1b
gbassisp <guilherme.bassi.sp@gmail.com>
2023-05-30 19:59:48 +0930

    test: added more cases

--- example/test/app/view/app_test.dart
+++ example/test/app/view/app_test.dart
@@ -9,6 +9,60 @@ class NotAWidget {
   final String text;
 }
 
+class One extends StatelessWidget {
+  // expect_lint: avoid_string_literals_inside_widget
+  const One({super.key, this.defaultValue = 'default'});
+
+  // expect_lint: avoid_string_literals_inside_widget
+  final defaultString = 'a string';
+  final String defaultValue;
+
+  @override
+  Widget build(BuildContext context) {
+    // expect_lint: avoid_string_literals_inside_widget
+    const a = Text('Counter');
+
+    return Row(
+      children: [
+        Text(defaultString),
+        Text(defaultValue),
+        a,
+      ],
+    );
+  }
+}
+
+class Another extends StatefulWidget {
+  // expect_lint: avoid_string_literals_inside_widget
+  const Another({super.key, this.defaultValue = 'default'});
+
+  final String defaultValue;
+
+  @override
+  State<Another> createState() => _AnotherState();
+}
+
+class _AnotherState extends State<Another> {
+  // expect_lint: avoid_string_literals_inside_widget
+  final defaultString = 'a string';
+
+  @override
+  Widget build(BuildContext context) {
+    // expect_lint: avoid_string_literals_inside_widget
+    const a = Text('Counter');
+
+    return Row(
+      children: [
+        Text(defaultString),
+        Text(widget.defaultValue),
+        a,
+      ],
+    );
+  }
+}
+
+const constString = 'a string';
+
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
@@ -22,7 +76,11 @@ void main() {
       // expect_lint: avoid_string_literals_inside_widget
       final w = Builder(builder: (_) => const Text('Counter'));
 
+      // not_expect_lint: avoid_string_literals_inside_widget
       const n = NotAWidget('text');
+
+      // not_expect_lint: avoid_string_literals_inside_widget
+      const widgetWithConstString = Text(constString);
 
       // not_expect_lint: avoid_string_literals_inside_widget
       final a = Builder(
@@ -31,8 +89,16 @@ void main() {
         ),
       );
 
+      Widget getWidget() {
+        // expect_lint: avoid_string_literals_inside_widget
+        return const Text('Counter');
+      }
+
       expect(a, isNotNull);
       expect(w, isNotNull);
+      expect(n, isNotNull);
+      expect(widgetWithConstString, isNotNull);
+      expect(getWidget(), isNotNull);
     });
   });
 }
