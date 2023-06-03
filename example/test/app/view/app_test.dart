fe47ff3b6d9a7f747cf7166702e330bc364d0b68
gbassisp <guilherme.bassi.sp@gmail.com>
2023-06-03 10:16:50 +0930

    fix: suppress intentional wrong declaration

--- example/test/app/view/app_test.dart
+++ example/test/app/view/app_test.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_field_initializers_in_const_classes
+
 import 'package:example/app/app.dart';
 import 'package:example/counter/counter.dart';
 import 'package:example/l10n/l10n.dart';
