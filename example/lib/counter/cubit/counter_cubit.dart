6da4f7ab6ea02a9a534b22b03e008396e4814d4a
guilherme <guilherme.bassi.sp@gmail.com>
2023-10-25 20:25:11 +1030

    test: added cases for new linter rule

--- example/lib/counter/cubit/counter_cubit.dart
+++ example/lib/counter/cubit/counter_cubit.dart
@@ -8,4 +8,32 @@ class CounterCubit extends Cubit<int> {
 
   void increment() => emit(state + 1);
   void decrement() => emit(state - 1);
+
+  // the following examples are test cases to enfore using switch
+  // statements on enum;
+  // dart has exhaustive_cases rule to ensure enum logic is compile safe
+
+  bool badConditionalOnEnum(TestCase example) {
+    // expect_lint: avoid_if_with_enum
+    if (example == TestCase.a) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  bool goodConditionalOnEnum(TestCase example) {
+    // not_expect_lint: avoid_if_with_enum
+    switch (example) {
+      case TestCase.a:
+        return true;
+      case TestCase.b:
+        return true;
+    }
+  }
+}
+
+enum TestCase {
+  a,
+  b,
 }
